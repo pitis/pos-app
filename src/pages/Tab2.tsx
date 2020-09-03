a4dc16b905949f0fc0dc5e25891f2bb11b03794c
Pitis <pitis.radu@gmail.com>
2020-09-03 06:44:04 +0300

    made without api call

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,13 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar, IonToast, IonItem, IonText, IonList, IonLabel, IonNote, IonButton } from '@ionic/react';
+import { v4 as uuidv4 } from 'uuid'
+import { trashOutline, pricetagOutline } from 'ionicons/icons'
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonSearchbar,
+  IonToast,
+  IonItem,
+  IonText,
+  IonList,
+  IonLabel,
+  IonNote,
+  IonButton,
+  IonModal,
+  IonRange,
+  IonIcon,
+} from '@ionic/react'
 
 const Tab2: React.FC = () => {
-  const [searchText, setSearchText] = useState<string>('');
-  const [counter, setCounter] = useState<number>(1);
-  const [products, setProducts] = useState<[]>([]);
-  const [productsOnReceipt, setProductsOnReceipt] = useState<[]>([]);
+  const [searchText, setSearchText] = useState<string>('')
+  const [counter, setCounter] = useState<number>(1)
+  const [products, setProducts] = useState<any>([])
+  const [filteredProducts, setFilteredProducts] = useState<any>([])
+  const [productsOnReceipt, setProductsOnReceipt] = useState<any>([])
   const [showToast, setShowToast] = useState<boolean>(false)
+  const [reducereProcent, setReducereProcent] = useState<number>(0)
+  const [showModal, setShowModal] = useState<boolean>(false)
 
   useEffect(() => {
     async function getProducts() {
@@ -16,6 +38,7 @@ const Tab2: React.FC = () => {
         ,"c":"097532","met":"Produse","act":"Get"}`)
         console.log(productsRes.data.result)
         setProducts(productsRes.data.result)
+        setFilteredProducts(productsRes.data.result)
       } catch (error) {
         console.error(error)
       }
@@ -23,61 +46,168 @@ const Tab2: React.FC = () => {
     getProducts()
   }, [])
 
+  useEffect(() => {
+    if (products) {
+      setFilteredProducts(
+        products.filter(
+          (product: { Denumire: string }) =>
+            product.Denumire.toUpperCase().indexOf(searchText.toUpperCase()) >
+            -1
+        )
+      )
+    }
+  }, [searchText])
+
   return (
     <IonPage>
-      <IonHeader >
+      <IonContent>
+        <IonModal isOpen={showModal}>
+          <IonText>Alege procentul de reducere</IonText>
+          <IonItem>
+            <IonRange
+              pin={true}
+              min={0}
+              max={30}
+              value={reducereProcent}
+              onIonChange={(e) => setReducereProcent(e.detail.value as number)}
+            />
+          </IonItem>
+          <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
+        </IonModal>
+        <IonButton onClick={() => setShowModal(true)}>Show Modal</IonButton>
+      </IonContent>
+      <IonHeader>
         <IonToolbar color='tertiary'>
           <IonTitle>Adauga bon</IonTitle>
         </IonToolbar>
       </IonHeader>
       <div style={{ display: 'flex', height: '100%', background: '#999' }}>
-
         <div style={{ width: '70%', height: '100%' }}>
           <IonContent color='medium' style={{ width: '98%' }}>
             <IonToast
               isOpen={showToast}
               onDidDismiss={() => setShowToast(false)}
-              message="Your settings have been saved."
+              message='Your settings have been saved.'
               duration={3000}
             />
-            <IonHeader collapse="condense">
+            <IonHeader collapse='condense'>
               <IonToolbar>
-                <IonTitle size="large">Adauga bon</IonTitle>
+                <IonTitle size='large'>Adauga bon</IonTitle>
               </IonToolbar>
             </IonHeader>
             <div style={{ display: 'flex' }}>
-              <IonSearchbar style={{ width: '80%' }} value={searchText} onIonChange={e => setSearchText(e.detail.value!)}></IonSearchbar>
-              <div className="counter">
-                <div className="counter__inner">
-                  <button onClick={() => { if (counter !== 1) setCounter(counter - 1) }}>-</button>
-                  <IonText color="primary">{counter}</IonText>
+              <IonSearchbar
+                style={{ width: '80%' }}
+                value={searchText}
+                placeholder={'Cauta produse...'}
+                onIonChange={(e) => setSearchText(e.detail.value!)}
+              ></IonSearchbar>
+              <div className='counter'>
+                <div className='counter__inner'>
+                  <button
+                    onClick={() => {
+                      if (counter !== 1) setCounter(counter - 1)
+                    }}
+                  >
+                    -
+                  </button>
+                  <IonText color='primary'>{counter}</IonText>
                   <button onClick={() => setCounter(counter + 1)}>+</button>
-
                 </div>
               </div>
             </div>
 
-            {products && products.map((elem: { Denumire: string }, index) => <IonItem key={index}>{elem.Denumire}</IonItem>)}
+            {filteredProducts &&
+              filteredProducts.map(
+                (
+                  elem: { Denumire: string; Pret_cu_TVA: string },
+                  index: number
+                ) => (
+                  <IonItem
+                    key={index}
+                    onClick={() => {
+                      setProductsOnReceipt([
+                        ...productsOnReceipt,
+                        {
+                          key: uuidv4(),
+                          Denumire: elem.Denumire,
+                          Pret_cu_TVA: elem.Pret_cu_TVA,
+                          count: counter,
+                        },
+                      ])
+                    }}
+                  >
+                    {elem.Denumire}
+                  </IonItem>
+                )
+              )}
           </IonContent>
         </div>
         <div style={{ width: '30%', height: '100%' }}>
-          <IonContent style={{ height: '94%' }}>
-            <IonList>
-              {products && products.map((elem: { Denumire: string, Pret_cu_TVA: number }, index) => {
-                return <IonItem key={index}>
-                  <IonNote slot="start">{index + 2}</IonNote>
-                  <IonLabel>{elem.Denumire} {elem.Pret_cu_TVA}RON</IonLabel>
-                  <IonNote slot="end">X</IonNote>
-                </IonItem>
-              })}
+          <IonContent style={{ height: '92%' }}>
+            <div slot='fixed' className='buttons-container'>
+              <button
+                className='delete-button'
+                onClick={() => setProductsOnReceipt([])}
+              >
+                <IonIcon icon={trashOutline} />
+              </button>
+              <button
+                className='reducere-button'
+                onClick={() => setShowModal(true)}
+              >
+                <IonIcon icon={pricetagOutline} />
+              </button>
+            </div>
+            <IonList style={{ marginTop: '1.5rem' }}>
+              {productsOnReceipt &&
+                productsOnReceipt.map(
+                  (
+                    elem: {
+                      key: string
+                      Denumire: string
+                      Pret_cu_TVA: string
+                      count: string
+                    },
+                    index: number
+                  ) => (
+                    <IonItem key={elem.key}>
+                      <IonNote slot='start'>{elem.count}</IonNote>
+                      <IonLabel>
+                        {elem.Denumire}{' '}
+                        {parseFloat(elem.Pret_cu_TVA).toFixed(2)}RON
+                      </IonLabel>
+                      <IonNote
+                        slot='end'
+                        onClick={() => {
+                          let receiptRef = [...productsOnReceipt]
+
+                          setProductsOnReceipt(
+                            receiptRef.filter(
+                              (elem2, index2) => index2 !== index
+                            )
+                          )
+                        }}
+                      >
+                        X
+                      </IonNote>
+                    </IonItem>
+                  )
+                )}
             </IonList>
           </IonContent>
-          <IonButton style={{}} expand="block" color='primary' onClick={() => console.log('incasare')}>Incaseaza</IonButton>
+          <IonButton
+            style={{}}
+            expand='block'
+            color='primary'
+            onClick={() => console.log('incasare')}
+          >
+            Incaseaza
+          </IonButton>
         </div>
-
       </div>
     </IonPage>
-  );
-};
+  )
+}
 
-export default Tab2;
+export default Tab2
